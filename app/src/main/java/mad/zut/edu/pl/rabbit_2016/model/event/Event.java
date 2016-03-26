5287f13041e14cc294641f25e411e59134ce85ad
BartoszKozajda <bartosz.kozajda@gmail.com>
2016-03-26 22:36:57 +0100

    Update Event model

--- app/src/main/java/mad/zut/edu/pl/rabbit_2016/model/event/Event.java
+++ app/src/main/java/mad/zut/edu/pl/rabbit_2016/model/event/Event.java
@@ -7,7 +7,7 @@ import com.google.gson.annotations.SerializedName;
 
 @Generated("org.jsonschema2pojo")
 public class Event {
-
+    
     @SerializedName("companyId")
     @Expose
     private String companyId;
@@ -26,131 +26,152 @@ public class Event {
     @SerializedName("time")
     @Expose
     private String time;
+    @SerializedName("room")
+    @Expose
+    private String room;
     @SerializedName("title")
     @Expose
     private String title;
 
     /**
-     * 
+     *
      * @return
-     *     The companyId
+     * The companyId
      */
     public String getCompanyId() {
         return companyId;
     }
 
     /**
-     * 
+     *
      * @param companyId
-     *     The companyId
+     * The companyId
      */
     public void setCompanyId(String companyId) {
         this.companyId = companyId;
     }
 
     /**
-     * 
+     *
      * @return
-     *     The data
+     * The data
      */
     public String getData() {
         return data;
     }
 
     /**
-     * 
+     *
      * @param data
-     *     The data
+     * The data
      */
     public void setData(String data) {
         this.data = data;
     }
 
     /**
-     * 
+     *
      * @return
-     *     The id
+     * The id
      */
     public String getId() {
         return id;
     }
 
     /**
-     * 
+     *
      * @param id
-     *     The id
+     * The id
      */
     public void setId(String id) {
         this.id = id;
     }
 
     /**
-     * 
+     *
      * @return
-     *     The speaker
+     * The speaker
      */
     public String getSpeaker() {
         return speaker;
     }
 
     /**
-     * 
+     *
      * @param speaker
-     *     The speaker
+     * The speaker
      */
     public void setSpeaker(String speaker) {
         this.speaker = speaker;
     }
 
     /**
-     * 
+     *
      * @return
-     *     The speakerPictureUrl
+     * The speakerPictureUrl
      */
     public String getSpeakerPictureUrl() {
         return speakerPictureUrl;
     }
 
     /**
-     * 
+     *
      * @param speakerPictureUrl
-     *     The speakerPictureUrl
+     * The speakerPictureUrl
      */
     public void setSpeakerPictureUrl(String speakerPictureUrl) {
         this.speakerPictureUrl = speakerPictureUrl;
     }
 
     /**
-     * 
+     *
      * @return
-     *     The time
+     * The time
      */
     public String getTime() {
         return time;
     }
 
     /**
-     * 
+     *
      * @param time
-     *     The time
+     * The time
      */
     public void setTime(String time) {
         this.time = time;
     }
 
     /**
-     * 
+     *
      * @return
-     *     The title
+     * The room
+     */
+    public String getRoom() {
+        return room;
+    }
+
+    /**
+     *
+     * @param room
+     * The room
+     */
+    public void setRoom(String room) {
+        this.room = room;
+    }
+
+    /**
+     *
+     * @return
+     * The title
      */
     public String getTitle() {
         return title;
     }
 
     /**
-     * 
+     *
      * @param title
-     *     The title
+     * The title
      */
     public void setTitle(String title) {
         this.title = title;
