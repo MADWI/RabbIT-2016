2bd89c1049641d3b7883f77b559de3867e554dc5
Michał Bednarski <bednarski.michal2@gmail.com>
2016-04-12 11:06:57 +0200

    Update sending ratings for new api

--- app/src/main/java/mad/zut/edu/pl/rabbit_2016/model/CompanyPostData.java
+++ app/src/main/java/mad/zut/edu/pl/rabbit_2016/model/CompanyPostData.java
@@ -5,14 +5,27 @@ package mad.zut.edu.pl.rabbit_2016.model;
  */
 public class CompanyPostData {
     private final int companyId;
-    private final byte[] opinions;
+    private final Ratings opinions;
     private final String deviceUUID;
     private final String key;
 
-    public CompanyPostData(int companyId, byte[] opinions, String deviceUUID, String key) {
+    public CompanyPostData(int companyId, Ratings opinions, String deviceUUID, String key) {
         this.companyId = companyId;
         this.opinions = opinions;
         this.deviceUUID = deviceUUID;
         this.key = key;
     }
+
+    public static class Ratings {
+        byte op1;
+        byte op2;
+        byte op3;
+
+        public Ratings(byte op1, byte op2, byte op3) {
+            this.op1 = op1;
+            this.op2 = op2;
+            this.op3 = op3;
+        }
+    }
+
 }
