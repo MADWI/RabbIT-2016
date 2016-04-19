44f86edaab9b8296e0718d6bddde8ec2fc0fc814
BartoszKozajda <bartosz.kozajda@gmail.com>
2016-04-19 11:12:01 +0200

    Make links on authors page clickable

--- app/src/main/java/mad/zut/edu/pl/rabbit_2016/fragments/AuthorsFragment.java
+++ app/src/main/java/mad/zut/edu/pl/rabbit_2016/fragments/AuthorsFragment.java
@@ -1,10 +1,13 @@
 package mad.zut.edu.pl.rabbit_2016.fragments;
 
 import android.app.Fragment;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import mad.zut.edu.pl.rabbit_2016.R;
 
@@ -18,6 +21,18 @@ public class AuthorsFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         View v = inflater.inflate(R.layout.authors, container, false);
+        v.findViewById(R.id.textView8).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://mad.zut.edu.pl/")));
+            }
+        });
+        v.findViewById(R.id.textView10).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com/MADWIZUT")));
+            }
+        });
         return v;
     }
 }
